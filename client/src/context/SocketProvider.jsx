e60decd9591918e6b65f68974587653ce95d1fd1
Alok Pal <alokpal28071997@gmail.com>
2024-11-07 10:55:49 +0530

    node is added in place of nodemon

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -8,7 +8,7 @@ const useSocket = () => {
 };
 
 const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"),[]);
+  const socket = useMemo(() => io(process.env.REACT_APP_BASE_URL),[]);
   return (
     <SocketContext.Provider value={socket}>
       {props.children}
